4e08c6b11c8f917a637386a01dc50748c8cce656
MOUGHIT <abdelmoughith2002@gmail.com>
2025-01-26 15:34:50 +0100

    now with folder for every chapter

--- manga.py
+++ manga.py
@@ -1,16 +1,17 @@
 import requests, os, bs4, re
 
-os.makedirs('manga', exist_ok=True)
+
 
 url = input('(download will start from current url \n'
             'to the last chapter you can stop any time\nprevious downloaded chapter won\'t be deleted)\n'
-            'paste the link of the manga from ANIMELEK : \n')
+            'paste the link of the manga from ANIMELEK with "/" at the end: \n')
 
 
 # fct that download all pages from
-def downloadMangaFromSinglePage(imageLinks:list):
+def downloadMangaFromSinglePage(imageLinks:list, folder_number:int):
+    os.makedirs(f'manga {folder_number}', exist_ok=True)
     for link in imageLinks:
-        imageFile = open(os.path.join('manga', os.path.basename(link)), 'wb')
+        imageFile = open(os.path.join(f'manga {folder_number}', os.path.basename(link)), 'wb')
         response = requests.get(link)
         for chunk in response.iter_content(100000):
             imageFile.write(chunk)
@@ -27,7 +28,6 @@ def ends_with_number(string):
 
 
 
-
 while ends_with_number(url):
     print(f'Downloading chapter {url}...')
     res = requests.get(url)
@@ -41,5 +41,5 @@ while ends_with_number(url):
     imageElem = soup.select('div.page-break.no-gaps img')
 
     links = list(map(lambda x: x.get('src'), imageElem))
-    downloadMangaFromSinglePage(links)
+    downloadMangaFromSinglePage(links, url[-3:-1])
     url = goToNextPage()
